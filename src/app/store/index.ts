5a8cc0d5f8bd864c2afce92a2d5fb82a193c22a4
devpav <jsdeveloper@yahoo.com>
2019-02-26 22:50:36 +0300

    Changed structure and added new components folder.

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -6,10 +6,12 @@ import * as type from './reducers/type.reducer';
 import * as tag from './reducers/tag.reducer';
 import * as language from './reducers/language.reducer';
 import {typeServiceReducer} from './reducers/type-service.reducer';
+import * as attraction from './reducers/attraction.reducer';
 
 export const reducers: ActionReducerMap<any> = {
   types: type.typeReducer,
   tags: tag.tagReducer,
   typeservises: typeServiceReducer,
-  language: language.languageReducer
+  language: language.languageReducer,
+  attractions: attraction.attractionReducer
 };
