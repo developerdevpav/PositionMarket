00647d3ac4a92f46caab33063221ec4989fcf8d5
devpav <jsdeveloper@yahoo.com>
2019-04-19 00:55:21 +0300

    Changed catalog page.

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {ApiTagLoadAll} from '../../store/actions/tag.actions';
+import {selectTagsByLanguage} from '../../store/selectors/tag.selectors';
+import {ApiTypeLoadAll} from '../../store/actions/type.actions';
+import {selectTypesByLanguage} from '../../store/selectors/type.selectors';
 
 @Component({
   selector: 'app-catalog',
@@ -7,12 +12,48 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CatalogComponent implements OnInit {
 
-  ex = true;
+  dropdownSelectTag: { id: any, title: string }[] = [];
+  dropdownSelectType: { id: any, title: string }[] = [];
 
-  constructor() {
+  dropdownAllTag: { id: any, title: string }[] = [];
+  dropdownAllType: { id: any, title: string }[] = [];
+
+  settingsTag = {
+    text: 'Теги аттракциона',
+    selectAllText: 'Выделить все',
+    unSelectAllText: 'Снять',
+    enableSearchFilter: true,
+    singleSelection: true,
+    labelKey: 'title'
+  };
+
+  settingsType = {
+    text: 'Типы аттракциона',
+    selectAllText: 'Выделить все',
+    unSelectAllText: 'Снять',
+    enableSearchFilter: true,
+    singleSelection: true,
+    labelKey: 'title'
+  };
+
+  constructor(private store: Store<any>) {
 
   }
 
   ngOnInit() {
+    this.store.dispatch(new ApiTagLoadAll());
+    this.store.dispatch(new ApiTypeLoadAll());
+
+    this.store.select(selectTagsByLanguage).subscribe(it => {
+      this.dropdownAllTag = it;
+    });
+
+    this.store.select(selectTypesByLanguage).subscribe(it => {
+      this.dropdownAllType = it;
+    });
+  }
+
+  createItemProduct() {
+
   }
 }
